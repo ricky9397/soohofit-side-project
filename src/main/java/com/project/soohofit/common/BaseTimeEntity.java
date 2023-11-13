f64c2663524e4b615c16db0cbbd0528345e38e64
ricky <kimzzan1234@gmail.com>
2023-11-13 23:42:26 +0900

    20231113-김명호-userEntity, 디자인 수정

---
+++ src/main/java/com/project/soohofit/common/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.project.soohofit.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @Column(name = "REG_ID", length = 30)
+    private String regId;
+
+    @CreatedDate
+    @Column(name = "REG_DT")
+    public LocalDateTime regDt;
+
+    @Column(name = "UPD_ID", length = 30)
+    private String updId;
+
+    @CreatedDate
+    @Column(name = "UPD_DT")
+    public LocalDateTime updDt;
+
+}
---
+++ src/main/webapp/WEB-INF/views/user/login.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+<%@ include file="../common/inc/header_inc.jsp"%>
+
+
+<%@ include file="../common/inc/footer_inc.jsp"%>
+
+<script>
+
+</script>
