3f27e413acd420dd5912040d0d493bcfcc253933
park <hansu0512@gamil.com>
2023-11-20 01:32:49 +0900

    20231119-박한수-redis session 추가

--- src/main/java/com/project/soohofit/main/controller/MainController.java
+++ src/main/java/com/project/soohofit/main/controller/MainController.java
@@ -6,7 +6,10 @@ import com.project.soohofit.common.response.ResponseStatus;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,9 @@ import java.util.Map;
 public class MainController {
 
     private Logger logger = LogManager.getLogger(MainController.class);
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
 
     @Value("${postgre.url}")
     private String url;
@@ -38,6 +44,12 @@ public class MainController {
     public CommonResponse ajax() {
         Map<String, Object> data = new HashMap<>();
         data.put("result", url);
+
+        ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+
+        stringValueOperations.set("hello", "world");
+
+        String redisValue = stringValueOperations.get("hello");
         return DataResponse.of(ResponseStatus.SUCCESS, data);
     }
 
