5f986ec1f9129e9646102aba2f46634d59165cee
ricky <kimzzan1234@gmail.com>
2023-11-22 00:44:50 +0900

    20231120-김명호-ExceptionHandle 추가

---
+++ src/main/java/com/project/soohofit/common/error/exception/BusinessBaseException.java
@@ -0,0 +1,21 @@
+package com.project.soohofit.common.error.exception;
+
+import com.project.soohofit.common.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessBaseException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessBaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessBaseException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
